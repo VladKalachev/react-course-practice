67f35c852a00d3e094e23794dcb6f70eec4df092
Vlad <vlvete1990@yandex.ru>
2022-11-09 17:47:14 +0300

    chore: jsx

--- 02-base-jsx/src/index.js
+++ 02-base-jsx/src/index.js
@@ -1,8 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+// import App from './App';
 import reportWebVitals from './reportWebVitals';
+
+const Book = ({name, year, price}) => {
+  return <div>
+    <h2>{name}</h2>
+    <p>{year}</p>
+    <p>{price}</p>
+  </div>
+}
+const App = () => {
+  return (
+    <div>
+      <h1>Hello Wordl!</h1>
+      <Book name={'JS for beginners'} year={2018} price={1000}/>
+      <Book name={'React'} year={2019} price={3000}/>
+      <Book name={'Vue'} year={2020} price={2000}/>
+    </div>
+  )
+}
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
